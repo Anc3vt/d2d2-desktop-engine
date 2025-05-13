aac2f6de4d150d44fa8763953f3c1f2ff5de7212
Ancevt <me@ancevt.com>
2025-05-14 00:12:10 +0600

    Add framework Logger interface Log & D2D2.log static field

--- src/main/java/com/ancevt/d2d2/engine/lwjgl/LwjglEngine.java
+++ src/main/java/com/ancevt/d2d2/engine/lwjgl/LwjglEngine.java
@@ -27,6 +27,7 @@ import com.ancevt.d2d2.event.InputEvent;
 import com.ancevt.d2d2.event.core.EventDispatcherImpl;
 import com.ancevt.d2d2.input.Mouse;
 import com.ancevt.d2d2.lifecycle.D2D2PropertyConstants;
+import com.ancevt.d2d2.log.Log;
 import com.ancevt.d2d2.scene.Renderer;
 import com.ancevt.d2d2.scene.Root;
 import com.ancevt.d2d2.scene.interactive.InteractiveManager;
@@ -116,6 +117,70 @@ public class LwjglEngine extends EventDispatcherImpl implements Engine {
     public void setCanvasSize(int width, int height) {
         canvasWidth = width;
         canvasHeight = height;
+    }
+
+    @Override
+    public Log log() {
+        //TODO: refactor
+        return new Log() {
+            private static final String EMPTY_STRING = "";
+            private int level = INFO;
+            private boolean colorized = true;
+
+            @Override
+            public void setLevel(int level) {
+                this.level = level;
+            }
+
+            @Override
+            public int getLevel() {
+                return level;
+            }
+
+            @Override
+            public void setColorized(boolean colorized) {
+                this.colorized = colorized;
+            }
+
+            @Override
+            public boolean isColorized() {
+                return colorized;
+            }
+
+            private void logMessage(Object tag, Object msg, PrintStream stream, Throwable throwable) {
+                String tagStr = (tag instanceof Class<?> clazz) ? clazz.getSimpleName() : String.valueOf(tag);
+                String formattedMsg = UnixTextColorFilter.filterText(String.valueOf(msg), colorized);
+                stream.printf("%s: %s%n", tagStr, formattedMsg);
+                if (throwable != null) {
+                    throwable.printStackTrace(stream);
+                }
+            }
+
+            @Override
+            public void error(Object tag, Object msg) {
+                if (level < ERROR) return;
+                logMessage(tag, msg, System.err, null);
+            }
+
+            @Override
+            public void error(Object tag, Object msg, Throwable throwable) {
+                if (level < ERROR) return;
+                logMessage(tag, msg, System.err, throwable);
+            }
+
+            @Override
+            public void info(Object tag, Object msg) {
+                if (level < INFO) return;
+                logMessage(tag, msg, System.out, null);
+            }
+
+            @Override
+            public void debug(Object tag, Object msg) {
+                if (level < DEBUG) return;
+                logMessage(tag, msg, System.out, null);
+            }
+        };
+
     }
 
     @Override
